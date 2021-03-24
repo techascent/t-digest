c4bd0cb584bdfdbfa068a3fc2cb27abcda8379f0
Chris Nuernberger <chris@techascent.com>
2021-03-24 13:39:32 -0600

    Adding stub project for quick release to clojars.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.techascent/t-digest "4.0-pre-release-2-SNAPSHOT"
+  :description "Pre release of tdunning 4.0 that fixes several major issues with
+t-digest."
+  :url "https://github.com/tdunning/t-digest"
+  :java-source-paths ["core/src/main/java"])
